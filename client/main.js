9e1ddbc9ba6a5c78563b17b6b7c544f35f9ae45e
Taryn King <marshaeking@gmail.com>
2022-12-02 17:15:29 -0600

    feat: add fortune button

--- client/main.js
+++ client/main.js
@@ -1,11 +1,22 @@
-const complimentBtn = document.getElementById("complimentButton")
+const complimentBtn = document.getElementById("complimentButton");
+
+const fortuneBtn = document.getElementById("fortuneButton");
 
 const getCompliment = () => {
     axios.get("http://localhost:4000/api/compliment/")
         .then(res => {
             const data = res.data;
-            alert(data);
-    });
+            swal("Hey...", data);
+        });
+};
+
+const getFortune = () => {
+    axios.get("http://localhost:4000/api/fortune/")
+        .then(res => {
+            const data = res.data;
+            swal("✨✨ Your fortune says...", data);
+        });
 };
 
 complimentBtn.addEventListener('click', getCompliment)
+fortuneBtn.addEventListener('click', getFortune)
--- server/controller.js
+++ server/controller.js
@@ -2,12 +2,25 @@ module.exports = {
 
     getCompliment: (req, res) => {
         const compliments = ["Gee, you're a smart cookie!", "Cool shirt!", "Your Javascript skills are stellar."];
-      
-        // choose random compliment
-        let randomIndex = Math.floor(Math.random() * compliments.length);
-        let randomCompliment = compliments[randomIndex];
-      
-        res.status(200).send(randomCompliment);
-    }
 
+        // choose random compliment
+        let randomIndex = randomGenerator(compliments);
+        let randomCompliment = compliments[randomIndex];
+
+        res.status(200).send(randomCompliment);
+    },
+
+    getFortune: (req, res) => {
+        const fortunes = ["A beautiful, smart, and loving person will be coming into your life.", "A truly rich life contains love and art in abundance.", "Do not underestimate yourself. Human beings have unlimited potentials.", "He who knows he has enough is rich.", "In order to take, one must first give."];
+
+        let randomIndex = randomGenerator(fortunes);
+        let randomFortune = fortunes[randomIndex];
+
+        res.status(200).send(randomFortune);
+
+    }
+}
+
+function randomGenerator(arr) {
+    return Math.floor(Math.random() * arr.length);
 }
